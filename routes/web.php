a8b293cc3941e539aa63fe7504b4719c3724ee06
viVo4s <stas22900@mail.ru>
2024-03-13 14:29:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Src\Route;
+
+Route::add('go', [Controller\Site::class, 'index']);
+Route::add('hello', [Controller\Site::class, 'hello']);
+    
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stas2/htdocs',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1e5ab9e2b618ea135ac3c831c3ea69d34b4acab0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'stas2/htdocs' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1e5ab9e2b618ea135ac3c831c3ea69d34b4acab0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
