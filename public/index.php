98413f04b88b11a86b41f5ee2d2e62c7d1f16a47
viVo4s <stas22900@mail.ru>
2024-03-13 21:51:11 +0700

    bug7

--- public/index.php
+++ public/index.php
@@ -11,3 +11,10 @@ try {
    print_r($exception);
    echo '</pre>';
 }
+
+stream_context_set_default([
+   'ssl' => [
+       'verify_peer' => false,
+       'verify_peer_name' => false,
+   ],
+]);
