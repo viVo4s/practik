76cec23ab5866e8623721390318ee679100e13c4
viVo4s <stas22900@mail.ru>
2024-03-14 22:40:44 +0700

    Registration function

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Model\Post;
 use Src\View;
 use Src\Request;
+use Model\User;
 
 class Site
 {
@@ -19,4 +20,12 @@ class Site
        return new View('site.hello', ['message' => 'hello working']);
    }
    
+   public function signup(Request $request): string
+   {
+       if ($request->method==='POST' && User::create($request->all())){
+           return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+       }
+       return new View('site.signup');
+   }
+
 }
