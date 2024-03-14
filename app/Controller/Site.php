aaa0b23d274523d5cf014adb1e8c261aa8ec7a85
viVo4s <stas22900@mail.ru>
2024-03-14 22:29:45 +0700

    Processing request data

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,17 +4,19 @@ namespace Controller;
 
 use Model\Post;
 use Src\View;
+use Src\Request;
 
 class Site
 {
-   public function index(): string
-   {
-       $posts = Post::all();
+    public function index(Request $request): string
+    {
+       $posts = Post::where('id', $request->id)->get();
        return (new View())->render('site.post', ['posts' => $posts]);
-   }
+    }
 
    public function hello(): string
    {
        return new View('site.hello', ['message' => 'hello working']);
    }
+   
 }
